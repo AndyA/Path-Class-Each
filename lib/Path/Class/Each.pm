4f52b45c4ae7b40a0a392e0c927f8e4a7774a676
Andy Armstrong <andy@hexten.net>
2010-09-18 15:19:31 +0100

    Bump version to 0.03

--- lib/Path/Class/Each.pm
+++ lib/Path/Class/Each.pm
@@ -6,7 +6,7 @@ use strict;
 use Carp qw( croak );
 use Path::Class;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 NAME
 
@@ -14,7 +14,7 @@ Path::Class::Each - Iterate lines in a file
 
 =head1 VERSION
 
-This document describes Path::Class::Each version 0.02
+This document describes Path::Class::Each version 0.03
 
 =head1 SYNOPSIS
 
