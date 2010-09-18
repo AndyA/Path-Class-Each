0dd0e52da39f9446832cbd722679edf8d2bdf74e
Andy Armstrong <andy@hexten.net>
2010-09-18 15:10:00 +0100

    Directory support.

--- lib/Path/Class/Each.pm
+++ lib/Path/Class/Each.pm
@@ -21,6 +21,8 @@ This document describes Path::Class::Each version 0.02
   use Path::Class;
   use Path::Class::Each;
 
+  ## Files ##
+
   # Iterator interface
   my $iter = file( 'foo', 'bar' )->iterator;
   while ( defined( my $line = $iter->() ) ) {
@@ -40,15 +42,44 @@ This document describes Path::Class::Each version 0.02
     }
   );
 
+  ## Directories ##
+  
+  # Iterator interface
+  my $iter = dir( 'foo', 'bar' )->iterator;
+  while ( defined( my $file = $iter->() ) ) {
+    print "File: $file\n";
+  }
+
+  # 'next' interface
+  my $file = dir( 'foo', 'bar' );
+  while ( defined( my $file = $dir->next_file ) ) {
+    print "File: $file\n";
+  }
+
+  # Callback interface
+  dir( 'foo', 'bar' )->each(
+    sub {
+      print "File: $_\n";
+    }
+  );
+
 =head1 DESCRIPTION
 
-C<Path::Class::Each> augments L<Path::Class::File> to provide three different
-ways of iterating over the lines of a file.
+C<Path::Class::Each> augments L<Path::Class::File> and
+L<Path::Class::Dir> to provide three different ways of iterating over
+the lines of a file and three ways of iterating the files recursively
+contained in a directory.
 
 C<Path::Class::File> provides a C<slurp> method that returns the
 contents of a file (either as a scalar or an array) but has no support
 for reading a file a line at a time. For large files it may be desirable
 to iterate through the lines; that's where this module comes in.
+
+C<Path::Class::Dir> provides C<children> which returns the files and
+directories immediately contained in a directory but does not expose a
+'find' interface to recursively search a directory. This module provides
+iterators that visit all the files in the directory tree below a
+directory.
 
 =head1 INTERFACE
 
@@ -136,19 +167,6 @@ That will create a new C<Path::Class::File> and, therefore, a new
 iterator each time it is called with the result that the first line of
 the file will be returned repeatedly.
 
-=cut
-
-sub Path::Class::File::next {
-  my $self = shift;
-
-  $self->{_iter} = $self->iterator( @_ )
-   unless $self->{_iter};
-
-  my $line = $self->{_iter}->();
-  delete $self->{_iter} unless defined $line;
-  return $line;
-}
-
 =head2 C<< Path::Class::File->each >>
 
 Call a supplied callback for each line in a file. The same options that
@@ -158,15 +176,75 @@ L<iterator> accepts may be passed:
 
 Within the callback the current line will be in C<$_>.
 
+=head2 C<< Path::Class::Dir::iterator >>
+
+Return an iterator that returns each of the files in and below a
+directory.
+
 =cut
 
-sub Path::Class::File::each {
+sub Path::Class::Dir::iterator {
   my ( $self, @opt ) = @_;
-  my $cb = pop @opt;
 
-  my $iter = $self->iterator( @opt );
-  while ( defined( local $_ = $iter->() ) ) {
-    $cb->();
+  croak "each requires a number of name => value options"
+   if @opt % 2;
+
+  my @queue = $self->children( @opt );
+  return sub {
+    TRY: {
+      return unless @queue;
+      my $obj = shift @queue;
+      if ( $obj->isa( 'Path::Class::Dir' ) ) {
+        unshift @queue, $obj->children( @opt );
+        redo TRY;
+      }
+      return $obj;
+    }
+  };
+}
+
+=head2 C<< Path::Class::Dir::next_file >>
+
+Return the next file from a recursive search of a directory. Returns
+C<undef> when all lines have been read.
+
+Internally L<iterator> is called if necessary to create a new iterator.
+The same options that L<iterator> accepts may be passed to C<next_file>:
+
+  my $dir = dir( 'foo', 'bar' );
+  while ( defined( my $file = $dir->next_file ) ) {
+    print "File: $file\n";
+  }
+
+=head2 C<< Path::Class::Dir::each >>
+
+Call a supplied callback for each file in a directory. The same options
+that L<iterator> accepts may be passed:
+
+  dir( 'foo', 'bar' )->each( sub { print "File: $_\n" } );
+
+Within the callback the current file will be in C<$_>.
+
+=cut
+
+BEGIN {
+  my @extend = qw( Path::Class::File Path::Class::Dir );
+  for my $class ( @extend ) {
+    no strict 'refs';
+    *{"${class}::each"} = sub {
+      my ( $self, @opt ) = @_;
+      my $cb   = pop @opt;
+      my $iter = $self->iterator( @opt );
+      while ( defined( local $_ = $iter->() ) ) { $cb->() }
+    };
+    my $next = $class eq 'Path::Class::Dir' ? 'next_file' : 'next';
+    *{"${class}::${next}"} = sub {
+      my $self = shift;
+      $self->{_iter} = $self->iterator( @_ ) unless $self->{_iter};
+      my $line = $self->{_iter}->();
+      delete $self->{_iter} unless defined $line;
+      return $line;
+    };
   }
 }
 
--- t/each.t
+++ t/each.t
@@ -2,7 +2,7 @@
 
 use strict;
 use warnings;
-use Test::More tests => 6;
+use Test::More tests => 9;
 
 use Path::Class;
 use Path::Class::Each;
@@ -38,3 +38,37 @@ for my $opt ( [], [ chomp => 1 ] ) {
     is_deeply \@got, \@want, "lines OK: next";
   }
 }
+
+{
+  my $dir = dir( 't', 'data' );
+  my @want = (
+    file( 't', 'data', 'foo', 'f1' ) . '',
+    file( 't', 'data', 'foo', 'f2' ) . '',
+    file( 't', 'data', 'lines' ) . '',
+  );
+
+  {
+    my @got  = ();
+    my $iter = $dir->iterator;
+    while ( defined( my $file = $iter->() ) ) {
+      push @got, $file;
+    }
+
+    is_deeply [ sort @got ], \@want, 'dir: iterator';
+  }
+
+  {
+    my @got = ();
+    $dir->each( sub { push @got, $_ } );
+    is_deeply [ sort @got ], \@want, 'dir: each';
+  }
+  {
+    my @got = ();
+
+    while ( defined( my $file = $dir->next_file ) ) {
+      push @got, $file;
+    }
+    is_deeply [ sort @got ], \@want, 'dir: next_file';
+  }
+}
+
